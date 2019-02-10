a573fdaf7350d0585212867fe22505985d4c1063
PydPiper <vkisforever@gmail.com>
2019-02-09 21:05:22 -0800

    added doc string to fizzbuzz

--- 02_FizzBuzz_Grid_Fib/fizzbuzz.py
+++ 02_FizzBuzz_Grid_Fib/fizzbuzz.py
@@ -9,6 +9,13 @@ Objective: From the count of 0 to 100 print Fizz on numbers divisible of 3, Buzz
 
 
 def fizzbuzz(start_no: int = 0, stop_no: int = 100) -> None:
+    """
+    Classic FizzBuzz challenge. Marks numbers divisible by 3 with Fizz, 5 with Buzz, both with FizzBuzz.
+
+    :param start_no: (type:int, default=0) Start number for loop up from +1 at a time
+    :param stop_no: (type:int, default=100) Stop number (last number is included in the check for FizzBuzz
+    :return: (type:str) Use print(fizzbuzz()) to output to console 
+    """
     current_value = start_no
     buffer = ""
     while current_value <= stop_no:
